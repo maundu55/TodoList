03e2ce74eda5e1353ede3cc5980880d381a2a5b3
maundu55 <marymaundu.mm@gmail.com>
2024-11-23 13:49:50 +0300

    a simple todo project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import TodoList from './TodoList'
+
+
+function App() {
+
+
+  return (
+    <>
+   <TodoList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TodoList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import "./style.css"
+
+const TodoList = () => {
+
+    const[todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+
+    const handleSubmit =()=>{
+        setTodos((todos)=>{
+            todos.concat({
+                text:input,
+                id: Math.floor(Math.random()*10),
+            });
+        });
+        setInput('');
+    }
+
+  return (
+    <div className="container">
+        <input type="text" placeholder="New Todo" value={input} onChange={(e)=> setInput(e.target.value)}/>
+
+        <button onClick={handleSubmit}>Submit</button>
+
+        <ul className="todos-list">
+            {todos.map(({text, id})=>(
+                <li className="todo" key={id}>
+                    <span>{text}</span>
+                    <button className="close" onClick={()=> removeTodo(id)}>X</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList
